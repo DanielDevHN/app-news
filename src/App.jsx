e57736c9c62342f73addb5d0a659b9a0557d4360
DanielDevHN <dangrereyes@gmail.com>
2022-08-26 15:13:26 -0600

    Agregando estructura al container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Container, Grid, Typography } from "@mui/material"
+import Formulario from "./components/Formulario"
 
 function App() {
 
 
   return (
-    <div>App News</div>
+    <Container>
+        <header>
+          <Typography align='center' marginY={5} component="h1" variant="h3">
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+    </Container>
   )
 }
 
