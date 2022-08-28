ed21ba7153391ad34ab3b7443a5c6a522ce59fe7
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 13:08:48 -0600

    Actualizando componente para listar noticias

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,13 +1,35 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import useNews from '../hooks/useNews'
+import Noticia from './Noticia'
 
 
 const ListadoNoticias = () => {
+    const { noticias } = useNews()
+
+    console.log(noticias)
+
   return (
     <>
-        <Typography>
-            Ultimas Noticias
+        <Typography
+            textAlign={'center'}
+            marginY={5}
+            variant='h3'
+            component={'h2'}
+        >
+            Últimas Noticias
         </Typography>
+
+        <Grid
+            container
+            spacing={2}
+        >
+            {noticias.map(noticia => (
+                <Noticia 
+                    noticia={noticia}
+                />
+            ))}
+        </Grid>
     </>
   )
 }
