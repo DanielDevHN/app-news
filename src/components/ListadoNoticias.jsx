c88fe6a53b90c0e8e95ad55c9a0ee70c8a722d9e
DanielDevHN <dangrereyes@gmail.com>
2022-08-27 21:19:25 -0600

    Agregando componente para mostrar noticias

---
+++ src/components/ListadoNoticias.jsx
@@ -0,0 +1,15 @@
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+
+const ListadoNoticias = () => {
+  return (
+    <>
+        <Typography>
+            Ultimas Noticias
+        </Typography>
+    </>
+  )
+}
+
+export default ListadoNoticias
