45e563f215ea2e6eee498a97c298d46cabb93886
DanielDevHN <dangrereyes@gmail.com>
2022-08-27 21:19:52 -0600

    Agregando contexto para consumir api

---
+++ src/context/NewsProvider.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState, useEffect, createContext } from "react";
+
+
+const NewsContext = createContext()
+
+
+
+const NewsProvider = ({ children }) => {
+
+    const [categoria, setCategoria] = useState('technology')
+    const [noticias, setNoticias] = useState([])
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+            const {data} = await axios(url)
+            setNoticias(data.articles)
+        }
+        consultarAPI()
+    }, [categoria])
+
+    const handleChangeCategoria = e => {
+        setCategoria(e.target.value)
+    }
+
+    return (
+        <NewsContext.Provider
+            value={{
+                categoria,
+                handleChangeCategoria
+            }}
+        >
+            {children}
+        </NewsContext.Provider>
+    )
+}
+
+
+export {
+    NewsProvider,
+}
+
+export default NewsContext
+
+
