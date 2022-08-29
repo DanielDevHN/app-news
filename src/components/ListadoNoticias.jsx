ceffe0e56c75400176d2b27379a2d0d444372182
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 18:46:53 -0600

    Se agregó paginación a las noticias

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,37 +1,58 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import Pagination from '@mui/material/Pagination'
+import Stack from '@mui/material/Stack'
 import useNews from '../hooks/useNews'
 import Noticia from './Noticia'
 
 
 const ListadoNoticias = () => {
-    const { noticias } = useNews()
+    const { noticias, totalNoticias, handleChangePagina, pagina } = useNews()
 
-    console.log(noticias)
+    const totalPaginas = Math.ceil(totalNoticias / 20)
 
-  return (
-    <>
-        <Typography
-            textAlign={'center'}
-            marginY={5}
-            variant='h3'
-            component={'h2'}
-        >
-            Últimas Noticias
-        </Typography>
+    return (
+        <>
+            <Typography
+                textAlign={'center'}
+                marginY={5}
+                variant='h3'
+                component={'h2'}
+            >
+                Últimas Noticias
+            </Typography>
 
-        <Grid
-            container
-            spacing={2}
-        >
-            {noticias.map(noticia => (
-                <Noticia 
-                    noticia={noticia}
+            <Grid
+                container
+                spacing={2}
+            >
+                {noticias.map(noticia => (
+                    <Noticia
+                        key={noticia.url}
+                        noticia={noticia}
+                    />
+                ))}
+            </Grid>
+
+            <Stack
+                sx={{
+                    marginY: 5
+                }}
+                spacing={2}
+                direction={'row'}
+                justifyContent='center'
+                alignItems='center'
+            >
+                <Pagination
+                    count={totalPaginas}
+                    color="primary"
+                    onChange={handleChangePagina}
+                    page={pagina}
                 />
-            ))}
-        </Grid>
-    </>
-  )
+            </Stack>
+
+        </>
+    )
 }
 
 export default ListadoNoticias
